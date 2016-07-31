e67a0c112947c024b732951fcd0bedbfc307b719
mateusz <mateusz@mlody>
2016-07-31 21:30:35 +0200

    fixing test for object deserializer

--- main.cpp
+++ main.cpp
@@ -129,9 +129,9 @@ using ObjectDeserializer = cxxJson::detail::ObjectDeserializer<ObjectType>;
 TEST(DeserializeTest, deserializerObject)
 {
     ptree json;
-    json.push_back(getChild(5, "i"));
-    json.push_back(getChild(true, "b"));
-    json.push_back(getChild("str", "str"));
+    json.put("i", 5);
+    json.put("b", true);
+    json.put("str", "str");
     auto out = ObjectDeserializer<TestTypes::FooObject>::deserialize(json);
     TestTypes::FooObject expected{5, true, "str"};
     assert(out == expected);
